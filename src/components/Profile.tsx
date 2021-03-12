dda8d8a8a8c1a0926b3bcda4b43813330d6cfe59
Guilherme Gregory <guilhermegreegory@gmail.com>
2021-03-12 12:23:28 -0300

    Aula 04

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,6 +1,12 @@
+import { useContext } from 'react';
+
+import { ChallengesContext } from '../contexts/ChallengesContext';
+
 import styles from '../styles/components/Profile.module.css';
 
 export function Profile() {
+  const { level } = useContext(ChallengesContext);
+
   return (
     <div className={styles.profileContainer}>
       <img src="https://avatars.githubusercontent.com/u/62723388?s=460&u=e4300cecb4edc4214f1a6dd74afad87e5962553e&v=4" alt="Guilherme Gregory" />
@@ -8,8 +14,8 @@ export function Profile() {
         <strong>Diego Fernandes</strong>
         <p>
           <img src="icons/level.svg" alt="Level" />
-          Level 1
-          </p>
+          Level {level}
+        </p>
       </div>
     </div>
   );
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,7 @@
 import Head from "next/head";
 
+import { CountdownProvider } from "../contexts/CountdownContext";
+
 import { Profile } from "../components/Profile";
 import { ExperienceBar } from "../components/ExperienceBar";
 import { CompletedChallenges } from "../components/CompletedChallenges";
@@ -17,16 +19,18 @@ export default function Home() {
 
       <ExperienceBar />
 
-      <section>
-        <div>
-          <Profile />
-          <CompletedChallenges />
-          <Countdown />
-        </div>
-        <div>
-          <ChallengeBox />
-        </div>
-      </section>
+      <CountdownProvider>
+        <section>
+          <div>
+            <Profile />
+            <CompletedChallenges />
+            <Countdown />
+          </div>
+          <div>
+            <ChallengeBox />
+          </div>
+        </section>
+      </CountdownProvider>
     </div>
   );
 };
